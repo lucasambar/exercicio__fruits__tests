2c2f98cf0e8ee0177cee719e544a11670b0fe428
Lucas Ambar <lucass.ambar@gmail.com>
2023-02-02 10:30:20 -0300

    first configurations

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { json, Request, Response } from "express";
+import fruitsRouter from "./routers/fruits-router";
+
+export const app = express();
+app.use(json());
+
+app.get("/health", (req: Request, res: Response) => res.send("I'am alive!"));
+app.use(fruitsRouter);
+
